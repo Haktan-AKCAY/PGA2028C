88de5d0b8dd510717f334a2c32179719f313a6b0
Haktan-AKCAY <haktanbatuhanakcay@gmail.com>
2025-03-21 15:07:18 +0300

    LAB - Etkinlik 03
    LAB - Etkinlik 03

---
+++ lab03_fonksiyonlarVeDonguler.py
@@ -0,0 +1,19 @@
+def pi(): #parametre almayan değer döndüren fonksiyon
+    return 3.14
+
+def cemberinCevresi(yaricap): #parametre alan ve değer döndüren fonksiyon
+    return 2*pi()*yaricap
+def reklam(): #parametre almayan ve değer döndürmeyen fonksiyon
+    mesaj = "Bu okulun en çalışkan sınıfı 9C sınıfıdır!"
+    print(len(mesaj)*"-")
+    print(mesaj)
+    print(len(mesaj)*"-")
+
+def sinifBas(x, y):
+    for say in range(y):
+        print((say+1)*(x+" "))
+        
+r = int(input("Çembere ait yarıçap uzunluğu giriniz:"))
+print("Yarıçapı", r, "olan çemberin çevresi", cemberinCevresi(r))
+sinifBas("9C", 5)
+reklam()
